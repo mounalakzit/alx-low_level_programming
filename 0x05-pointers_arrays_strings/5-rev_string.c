86bae07feb6f264b193df63de58c9a5b69e230cf
MOUNA <lakzitmouna83@gmail.com>
2023-09-19 08:09:08 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* rev_string - function thet print string
+* in reserved mode
+* @s:th string input
+* Return: Nothing
+*/
+void rev_string(char *s)
+{
+int length, i;
+char temp;
+length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+for (i = 0; i < length / 2; i++)
+{
+temp = s[i];
+s[i] = s[length - i - 1];
+s[length - i - 1] = temp;
+}
